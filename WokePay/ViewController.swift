9bd0892f7e938325d751eeeed4b5a1a3cf27a189
Rui Zhou <ruizhouzr@berkeley.edu>
2018-11-03 14:53:47 -0700

    Initial Commit

---
+++ WokePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WokePay
+//
+//  Created by zhou_rui on 11/3/18.
+//  Copyright © 2018 Rui Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WokePayTests/WokePayTests.swift
@@ -0,0 +1,34 @@
+//
+//  WokePayTests.swift
+//  WokePayTests
+//
+//  Created by zhou_rui on 11/3/18.
+//  Copyright © 2018 Rui Zhou. All rights reserved.
+//
+
+import XCTest
+@testable import WokePay
+
+class WokePayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
