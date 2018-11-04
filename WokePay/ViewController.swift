abb35efadcd0d87f10ac1c26a199e9ada6741a9c
Alan He <xhe22@berkeley.edu>
2018-11-03 19:23:59 -0700

    Added some comments lines

--- WokePay/ViewController.swift
+++ WokePay/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: Actions
 }
 
